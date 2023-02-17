79a915af1baf23100075f1bfdcf8b1245a30cb33
VuHoangB3 <lemaivuhoangdm2001@gmail.com>
2023-02-17 18:44:48 +0700

    add Project B3

---
+++ game.js
@@ -0,0 +1,72 @@
+const cvs = document.getElementById("bird");
+const ctx = cvs.getContext("2d");
+
+let frames = 0;
+
+
+const sprite = new Image();
+sprite.src = "img/sprite.png";
+
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y: cvs.height - 226,
+
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+
+}
+
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+
+    dx: 2,
+
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    },
+
+    update: function () {
+        if (state.current == state.game) {
+            this.x = (this.x - this.dx) % (this.w / 2);
+        }
+    }
+}
+
+function draw() {
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+    bg.draw();
+    fg.draw();
+
+}
+
+// UPDATE
+function update() {
+
+}
+
+// LOOP
+function loop() {
+    update();
+    draw();
+    frames++;
+
+    requestAnimationFrame(loop);
+}
+loop();
+
