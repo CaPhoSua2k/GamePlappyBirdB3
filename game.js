267b3fd68bffa0e8470360b430beeffd98498024
VuHoangB3 <lemaivuhoangdm2001@gmail.com>
2023-02-20 10:10:27 +0700

    fix loi (30p)

--- game.js
+++ game.js
@@ -22,28 +22,30 @@ const bg = {
     }
 
 }
+
+
 // GAME STATE
 const state = {
-    current : 0,
-    getReady : 0,
-    game : 1,
-    over :2
+    current: 0,
+    getReady: 0,
+    game: 1,
+    over: 2
 }
 // CONTROL THE GAME
-document.addEventListener("click",function(evt){
-    switch(state.current){
+cvs.addEventListener("click", function (evt) {
+    switch (state.current) {
         case state.getReady:
             state.current = state.game;
             break;
         case state.game:
-            birt.flap();
+            bird.flap();
             break;
         case state.over:
             state.current = state.getReady;
             break;
     }
 });
-// BACK GROUND
+
 const fg = {
     sX: 276,
     sY: 0,
@@ -66,69 +68,74 @@ const fg = {
         }
     }
 }
-const birt = {
-    Animation : [
-        {sX: 276, sY: 112},
-        {sX: 276, sY:139},
-        {sX: 276, sY:164},
-        {sX: 276, sY:164},
-    ],
-    x : 50,
-    y : 150,
-    w : 34,
-    h : 26,
 
-    frame : 0,
-    draw : function(){
-        let birt = this.Animation[this.frame];
-        ctx.drawImage(sprite, birt.sX, birt.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
+const bird = {
+    animation: [
+        { sX: 276, sY: 112 },
+        { sX: 276, sY: 139 },
+        { sX: 276, sY: 164 },
+        { sX: 276, sY: 139 }
+    ],
+    x: 50,
+    y: 150,
+    w: 34,
+    h: 26,
+
+    frame: 0,
+    draw: function () {
+        let bird = this.animation[this.frame];
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
     },
-    flap : function(){
-        if (state.current == state.getReady){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    flap: function () {
+        if (state.current == state.getReady) {
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         }
     }
 
 }
 
-//GET READY MESSAGE
 const getReady = {
-    sX : 0,
-    sY : 228,
-    w : 173,
-    h : 152,
-    x: cvs.with/2 - 173/2,
-    y : 80,
+    sX: 0,
+    sY: 228,
+    w: 173,
+    h: 152,
+    x: cvs.width / 2 - 173 / 2,
+    y: 80,
 
-    draw: function(){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);    
+    draw: function () {
+        if (state.current == state.getReady) {
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+
     }
 }
 // GAME OVER MESSAGE
 const gameOver = {
-    sX : 175,
-    sY : 228,
-    w : 225,
-    h : 202,
-    x: cvs.with/2 - 225/2,
-    y : 90,
+    sX: 175,
+    sY: 228,
+    w: 225,
+    h: 202,
+    x: cvs.width / 2 - 225 / 2,
+    y: 90,
 
-    draw: function(){
-        if(stat.currente == state.over){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);      
+    draw: function () {
+        if (state.current == state.over) {
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         }
     }
 }
-// DRAW
+
+
 function draw() {
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
-
     bg.draw();
     fg.draw();
-    birt.draw();
+    bird.draw();
     getReady.draw();
     gameOver.draw();
+
+
 }
 
 // UPDATE
@@ -145,5 +152,3 @@ function loop() {
     requestAnimationFrame(loop);
 }
 loop();
-
-
